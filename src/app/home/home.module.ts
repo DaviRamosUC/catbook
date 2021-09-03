97bedb8c7625c5dcf789f76be8d524477c180b1f
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2021-09-02 22:39:17 -0300

    Usando sistema de formTemplateDriven

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,16 +4,18 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FormsModule,
   ],
   exports: [HomeComponent]
 })
