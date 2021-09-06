b44b5415c89cf7abf98b1351b9a584e18a6b27e0
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2021-09-05 21:21:53 -0300

    gerando form de cadastro de usuario

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,3 +1,4 @@
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path: '',
         component: LoginComponent
+      },
+      {
+        path: 'novousuario',
+        component: NovoUsuarioComponent
       }
     ]
   },
