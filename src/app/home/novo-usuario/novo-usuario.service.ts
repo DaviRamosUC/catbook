8c9989dadaf249bfda1dc0297c354e459da53284
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2021-09-07 10:08:58 -0300

    novoUsuario e validação assincrona

--- src/app/home/novo-usuario/novo-usuario.service.ts
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -6,9 +6,13 @@ import { NovoUsuario } from './novo-usuario';
   providedIn: 'root'
 })
 export class NovoUsuarioService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   cadastraNovoUsuario(novoUsuario: NovoUsuario) {
-    return this.http.post('http://localhost:3000/user/signup', novoUsuario)
+    return this.http.post('http://localhost:3000/user/signup', novoUsuario);
+  }
+
+  verificaUsuarioExistente(nome: string) {
+    return this.http.get(`http://localhost:3000/user/exists/${nome}`);
   }
 }
